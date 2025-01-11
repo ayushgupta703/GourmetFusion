6a222cf998ca283ee80ad0d6a1ebb4572df02369
Ayush Gupta <ag6691602@gmail.com>
2025-01-12 00:55:40 +0530

    Navbar Created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar /> {/* Include the Navbar */}
+      {/* Other components and content will go here */}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    <a href="/">GourmetFusion</a>
+                </div>
+                <ul className={`navbar-links ${isOpen ? 'active' : ''}`}>
+                    <li><a href="#about">About Us</a></li>
+                    <li><a href="#menu">Menu</a></li>
+                    <li><a href="chef_word">Chef's Word</a></li>
+                    <li><a href="#gallery">Gallery</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                </ul>
+                <div className="navbar-toggle" onClick={toggleMenu}>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                    <span className="bar"></span>
+                </div>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar;
