52dc8c3bc91fc1a0a0512d89998a490136e1fff8
Ayush Gupta <ag6691602@gmail.com>
2025-01-18 21:43:40 +0530

    modification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 
 const App = () => {
   return (
     <div className="App">
-      <Navbar /> {/* Include the Navbar */}
-      {/* Other components and content will go here */}
+      <Navbar />
     </div>
   );
 };
