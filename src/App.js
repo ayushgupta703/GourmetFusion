020885777c10eb1f1b51fa8d19f74c1f987efd7f
Ayush Gupta <ag6691602@gmail.com>
2025-01-19 00:47:42 +0530

    Header Made Responsive & Started AboutUs Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
+import AboutUs from './Components/AboutUs/AboutUs';
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Header />
+      <AboutUs />
     </div>
   );
 };
---
+++ src/Components/AboutUs/AboutUs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './AboutUs.css';
+
+const AboutUs = () => (
+    <>
+        <div></div>
+    </>
+)
+
+export default AboutUs;
