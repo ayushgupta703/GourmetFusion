a106b27104b5f5442e59c4b80ec8e4209ba0a71a
Ayush Gupta <ag6691602@gmail.com>
2025-01-21 19:13:29 +0530

    Modified NavBar

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Header from './Components/Header/Header';
 import AboutUs from './Components/AboutUs/AboutUs';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 const App = () => {
   return (
+    // <Router>
     <div className="App">
       <Navbar />
       <Header />
       <AboutUs />
+      {/* <Switch>
+          <Route path="/about">
+            <AboutUs />
+          </Route>
+        </Switch> */}
     </div>
+    // </Router>
   );
 };
 
