18505771da24d5b0c36b83a5dcea1171baea1e9b
Ayush Gupta <ag6691602@gmail.com>
2025-01-28 16:12:39 +0530

    Created Menu And Chef's Word Page

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css'
+import { Link } from 'react-router-dom';
 
 const Header = () => (
     <>
@@ -8,7 +9,7 @@ const Header = () => (
                 <p>Chase The New Flavour</p>
                 <h1>The Key To Fine <br /> Dining</h1>
                 <p className='infoMsg'>Experience the art of fine dining with our expertly <br /> crafted dishes and exceptional service.</p>
-                <button><a href='#menu'>Explore Menu</a></button>
+                <button><Link to='/menu'>Explore Menu</Link></button>
             </div>
             <div className='info_img'>
                 <img src='https://b.zmtcdn.com/data/pictures/9/19106349/a89282f548ddd56866683ab4211a9bef.jpg?fit=around|750:500&crop=750:500;*,*' alt='infoImg.jpg'></img>
